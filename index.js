00b7113995a58394ead7952fdb2397756ad2ca6b
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-19 11:40:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// FileName: index.js
+
+
+// Import express
+let express = require('express')
+
+// Initialize the app
+let app = express();
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Wassim vous dis Hello World avec Express et Nodemon'));
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
+
+// Import routes
+let apiRoutes = require("./api-routes")// Use Api routes in the App
+app.use('/api', apiRoutes)
